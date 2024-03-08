ddd964b334d1eb2b69ac161570f5e5632f118b5c
Людмила <142536689+tangogenlu1@users.noreply.github.com>
2024-03-08 08:11:38 +0200

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,45 @@
+public class RandomDataGenerator {
+
+    public static void main(String[] args) {
+        }
+            data[i] = (int) (Math.random() * 100) + 1;
+            System.out.println("Random Number: " + item);
+        for (int i = 0; i < 10; i++) {
+        for (int item : data) {
+
+        int[] data = new int[10];
+        }
+    }
+}    data.forEach(item => {
+    for (let i = 0; i < 10; i++) {
+}
+
+        console.log("Random Number: " + item);
+    const data = [];
+    }
+
+}
+    return data;
+    const data = generateRandomData();
+    });
+function main() {
+main();
+function generateRandomData() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+    for (let i = 0; i < 10; i++) {
+    const data = [];
+    data.forEach(item => {
+    return data;
+    const data = generateRandomData();
+    });
+
+function main() {
+}
+main();
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+        console.log("Random Number: " + item);
+}
+    }
+function generateRandomData() {
